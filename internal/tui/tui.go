57b85371a253a5e390857a28f07445bef1c5c21d
brennantduck@gmail.com <brennantduck@gmail.com>
2025-12-08 10:26:23 -0500

    updated ui

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -3,7 +3,9 @@ package tui
 import (
 	"math"
 	"os"
+	"time"
 
+	"navitui/internal/mpv"
 	"navitui/internal/navidrome"
 
 	"github.com/charmbracelet/bubbles/table"
@@ -18,7 +20,7 @@ var (
 	headerStyle = lipgloss.NewStyle().
 			Bold(true).
 			Foreground(lipgloss.Color("57")).
-			MarginBottom(1)
+			Margin(1)
 
 	searchStyle = lipgloss.NewStyle().
 			MarginBottom(1).
@@ -26,6 +28,8 @@ var (
 			PaddingLeft(1).PaddingRight(1)
 
 	tableStyle = lipgloss.NewStyle().
+			MarginBottom(1).
+			PaddingLeft(1).PaddingRight(1).
 			BorderStyle(lipgloss.NormalBorder())
 )
 
@@ -41,31 +45,53 @@ var header = "" +
 	"   / | / /___ __   __(_)_  __/ / / /  _/\n" +
 	"  /  |/ / __ `/ | / / / / / / / / // /  \n" +
 	" / /|  / /_/ /| |/ / / / / / /_/ // /   \n" +
-	"/_/ |_/\\__,_/ |___/_/ /_/  \\____/___/\n"
+	"/_/ |_/\\__,_/ |___/_/ /_/  \\____/___/ \n"
+
+type (
+	onPlayFunc        func(id string) error
+	getPlayerStatus   func() (*mpv.Status, error)
+	tickNowPlayingMsg struct{}
+)
 
 type model struct {
 	focus  focus
 	search textinput.Model
 	table  table.Model
 
-	allTracks []navidrome.Track
-	filtered  []navidrome.Track
+	allSongs      []navidrome.Song
+	filteredSongs []navidrome.Song
 
-	nowPlaying navidrome.Track
-	isPlaying  bool
+	playerStatus mpv.Status
 
 	termHeight int
 	termWidth  int
+
+	onPlay        onPlayFunc
+	getNowPlaying getPlayerStatus
 }
 
 func (m model) Init() tea.Cmd {
-	return textinput.Blink
+	return tea.Batch(tickNowPlayingCmd(), textinput.Blink)
+}
+
+func tickNowPlayingCmd() tea.Cmd {
+	return tea.Tick(time.Second, func(time.Time) tea.Msg {
+		return tickNowPlayingMsg{}
+	})
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
+	case tickNowPlayingMsg:
+		var cmds []tea.Cmd
+		m.setPlayerStatus()
+
+		cmds = append(cmds, tickNowPlayingCmd())
+
+		return m, tea.Batch(cmds...)
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
@@ -97,13 +123,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 
 		case "enter":
-			if m.focus == focusTable && len(m.filtered) > 0 {
+			switch m.focus {
+			case focusTable:
 				idx := m.table.Cursor()
-				track := m.filtered[idx]
-				m.setNowPlaying(track)
-				m.isPlaying = true
-
-				return m, nil
+				song := m.filteredSongs[idx]
+				if err := m.setNowPlaying(song); err != nil {
+					return m, nil
+				}
+			case focusSearch:
+				song := m.filteredSongs[0]
+				if err := m.setNowPlaying(song); err != nil {
+					return m, nil
+				}
 			}
 		}
 	}
@@ -126,31 +157,45 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	header := headerStyle.Width(m.termWidth).Render(header)
+	header := headerStyle.
+		Align(lipgloss.Center).
+		Width(m.termWidth).
+		Render(header)
 
-	searchView := searchStyle.BorderForeground(lipgloss.Color("240")).Width(m.termWidth).Render(m.search.View())
+	searchView := searchStyle.
+		BorderForeground(lipgloss.Color("240")).
+		Width(m.termWidth).
+		Render(m.search.View())
 	if m.focus == focusSearch {
-		searchView = searchStyle.BorderForeground(lipgloss.Color("40")).Width(m.termWidth).Bold(true).Render(m.search.View())
+		searchView = searchStyle.
+			BorderForeground(lipgloss.Color("40")).
+			Width(m.termWidth).
+			Bold(true).
+			Render(m.search.View())
 	}
 
-	tableView := tableStyle.BorderForeground(lipgloss.Color("240")).Width(m.termWidth).Render(m.table.View())
+	tableView := tableStyle.
+		BorderForeground(lipgloss.Color("240")).
+		Width(m.termWidth).
+		Render(m.table.View())
+
 	if m.focus == focusTable {
-		tableView = tableStyle.BorderForeground(lipgloss.Color("40")).Width(m.termWidth).Render(m.table.View())
+		tableView = tableStyle.
+			BorderForeground(lipgloss.Color("40")).
+			Width(m.termWidth).
+			Render(m.table.View())
 	}
 
 	nowPlayingView := lipgloss.JoinVertical(
 		lipgloss.Center,
 		lipgloss.NewStyle().Width(m.termWidth).Render("Now Playing: "),
-		lipgloss.NewStyle().Width(m.termWidth).Render("Title: ", m.nowPlaying.Title),
-		lipgloss.NewStyle().Width(m.termWidth).Render("Artist: ", m.nowPlaying.Artist),
-		lipgloss.NewStyle().Width(m.termWidth).Render("Album: ", m.nowPlaying.Album),
+		lipgloss.NewStyle().Width(m.termWidth).Render("Title: ", m.playerStatus.Metadata.Title),
+		lipgloss.NewStyle().Render(m.playerStatus.Position.Round(time.Second).String()+" / "+m.playerStatus.Duration.Round(time.Second).String()),
+		lipgloss.NewStyle().Width(m.termWidth).Render("Artist: ", m.playerStatus.Metadata.Artist),
+		lipgloss.NewStyle().Width(m.termWidth).Render("Album: ", m.playerStatus.Metadata.Album),
 	)
 
-	views := []string{header, searchView, tableView}
-
-	if m.isPlaying {
-		views = append(views, nowPlayingView)
-	}
+	views := []string{header, searchView, tableView, nowPlayingView}
 
 	return lipgloss.JoinVertical(
 		lipgloss.Center,
@@ -158,7 +203,7 @@ func (m model) View() string {
 	) + "\n"
 }
 
-func newModel(tracks []navidrome.Track) model {
+func newModel(songs []navidrome.Song, op onPlayFunc, np getPlayerStatus) model {
 	width, height, err := term.GetSize(os.Stdout.Fd())
 	if err != nil {
 		width, height = 500, 500
@@ -172,41 +217,44 @@ func newModel(tracks []navidrome.Track) model {
 	ti.Width = 60
 
 	columns := []table.Column{
-		{Title: "Title", Width: int(math.Floor(float64(width) * 0.50))},
-		{Title: "Artist", Width: int(math.Floor(float64(width) * 0.20))},
-		{Title: "Album", Width: int(math.Floor(float64(width) * 0.15))},
-		{Title: "Genre", Width: int(math.Floor(float64(width) * 0.10))},
+		{Title: "Title", Width: getSize(width, .4)},
+		{Title: "Artist", Width: getSize(width, .2)},
+		{Title: "Album", Width: getSize(width, .15)},
+		{Title: "Genre", Width: getSize(width, .15)},
 	}
 
 	m := model{
-		focus:      focusSearch,
-		search:     ti,
-		allTracks:  tracks,
-		termHeight: height - 2,
-		termWidth:  width - 2,
+		focus:         focusSearch,
+		search:        ti,
+		allSongs:      songs,
+		termHeight:    height - 2,
+		termWidth:     width - 2,
+		onPlay:        op,
+		getNowPlaying: np,
 	}
 	m.buildTable(columns)
 
 	return m
 }
 
-func matchTrack(t navidrome.Track, q string) bool {
-	return fuzzy.Match(q, t.Title) || fuzzy.Match(q, t.Album) || fuzzy.Match(q, t.Artist) || fuzzy.Match(q, t.Genre)
+// use lithammer/fuzzy to find songs based on any attribute of the track. case-insensitive.
+func matchTrack(t navidrome.Song, q string) bool {
+	return fuzzy.MatchFold(q, t.Title) || fuzzy.MatchFold(q, t.Album) || fuzzy.MatchFold(q, t.Artist) || fuzzy.MatchFold(q, t.Genre)
 }
 
 // build table based on query
 func (m *model) buildTable(columns []table.Column) {
 	query := m.search.Value()
 
-	m.filtered = m.filtered[:0]
-	for _, t := range m.allTracks {
+	m.filteredSongs = m.filteredSongs[:0]
+	for _, t := range m.allSongs {
 		if query == "" || matchTrack(t, query) {
-			m.filtered = append(m.filtered, t)
+			m.filteredSongs = append(m.filteredSongs, t)
 		}
 	}
 
-	rows := make([]table.Row, len(m.filtered))
-	for i, t := range m.filtered {
+	rows := make([]table.Row, len(m.filteredSongs))
+	for i, t := range m.filteredSongs {
 		rows[i] = table.Row{t.Title, t.Artist, t.Album, t.Genre}
 	}
 
@@ -214,7 +262,7 @@ func (m *model) buildTable(columns []table.Column) {
 		table.WithColumns(columns),
 		table.WithRows(rows),
 		table.WithFocused(true),
-		table.WithHeight(10),
+		table.WithHeight(getSize(m.termHeight, .5)),
 		table.WithWidth(m.termWidth),
 	)
 
@@ -229,17 +277,36 @@ func (m *model) buildTable(columns []table.Column) {
 		Background(lipgloss.Color("57")).
 		Bold(false)
 	t.SetStyles(s)
-	t.SetStyles(s)
 
 	m.table = t
 }
 
-func Run(tracks []navidrome.Track) error {
-	m := newModel(tracks)
+func Run(songs []navidrome.Song, op onPlayFunc, np getPlayerStatus) error {
+	m := newModel(songs, op, np)
 	_, err := tea.NewProgram(m, tea.WithAltScreen()).Run()
 	return err
 }
 
-func (m *model) setNowPlaying(t navidrome.Track) {
-	m.nowPlaying = t
+// sets now playing to t
+func (m *model) setNowPlaying(t navidrome.Song) error {
+	if err := m.onPlay(t.ID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *model) setPlayerStatus() error {
+	status, err := m.getNowPlaying()
+	if err != nil {
+		return err
+	}
+
+	m.playerStatus = *status
+	return nil
+}
+
+// helper function to get size based on % of s. Used for m.termHeight or m.termWidth
+func getSize(s int, n float64) int {
+	return int(math.Floor(float64(s) * n))
 }
